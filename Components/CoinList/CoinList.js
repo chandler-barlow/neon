f1bb3771e8becc110cc2144cde5cbb2919d89613
Brisklemonade <71530812+Brisklemonade@users.noreply.github.com>
2021-10-02 13:48:35 -0500

    more ui updates

--- Components/CoinList/CoinList.js
+++ Components/CoinList/CoinList.js
@@ -1,5 +1,6 @@
 import styles from '../../styles/ComponentStyles/CoinList.module.css'
 import * as BiIcons from 'react-icons/bi'
+import * as IoIcons from 'react-icons/io'
 
 
 const CoinList = () => {
@@ -8,16 +9,16 @@ const CoinList = () => {
             <p>COINS</p>
             <div className={styles.coinBox}>
                 <div className={styles.symbol}>
-                    <BiIcons.BiBitcoin />
+                    <BiIcons.BiBitcoin style={{fontSize: 30}}/>
                 </div>
-                <div className={styles.leftContainer}>
+                <div className={styles.rightContainer}>
                     <div className={styles.topContainer}>
-                        <span>PRICE</span>
-                        <span>TICKER</span>
+                        <span style={{fontSize: 17}}>1.765</span>
+                        <span style={{fontSize: 12, fontWeight: 'bold'}}>BTC</span>
                     </div>
                     <div className={styles.bottomContainer}>
-                        <span>ARROW</span>
-                        <span>PERCENTAGE</span>
+                        <IoIcons.IoMdArrowDropup />
+                        <span style={{fontSize: 11}}>+12.5%</span>
                     </div>
                 </div>
             </div>
