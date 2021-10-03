a6396d325dc983960a7eea973053ce4090d237b6
collinmatz <collin.matz.a@gmail.com>
2021-10-03 02:04:45 -0500

    added api calls

---
+++ utilities/user_information.js
@@ -0,0 +1,132 @@
+const userData = [
+  {
+    "SSN": "dolor",
+    "email": "amet",
+    "Address": "Iola",
+    "Currency": {
+      "NeonQuant": 1,
+      "DollarQuant": 1
+    },
+    "LastName": "Aguilar",
+    "Username": "aliquip",
+    "FirstName": "Glover",
+    "PhoneNumber": "sit"
+  },
+  {
+    "SSN": "aliqua",
+    "email": "exercitation",
+    "Address": "Leyner",
+    "Currency": {
+      "NeonQuant": 7,
+      "DollarQuant": 2
+    },
+    "LastName": "Holden",
+    "Username": "ex",
+    "FirstName": "Parks",
+    "PhoneNumber": "nostrud"
+  },
+  {
+    "SSN": "incididunt",
+    "email": "duis",
+    "Address": "Summerset",
+    "Currency": {
+      "NeonQuant": 5,
+      "DollarQuant": 2
+    },
+    "LastName": "Schmidt",
+    "Username": "consequat",
+    "FirstName": "Gallegos",
+    "PhoneNumber": "laboris"
+  },
+  {
+    "SSN": "ad",
+    "email": "deserunt",
+    "Address": "Calverton",
+    "Currency": {
+      "NeonQuant": 3,
+      "DollarQuant": 3
+    },
+    "LastName": "Austin",
+    "Username": "labore",
+    "FirstName": "Amber",
+    "PhoneNumber": "non"
+  },
+  {
+    "SSN": "qui",
+    "email": "ut",
+    "Address": "Hardyville",
+    "Currency": {
+      "NeonQuant": 1,
+      "DollarQuant": 3
+    },
+    "LastName": "Gallagher",
+    "Username": "fugiat",
+    "FirstName": "Mcmillan",
+    "PhoneNumber": "duis"
+  },
+  {
+    "SSN": "nisi",
+    "email": "mollit",
+    "Address": "Stockdale",
+    "Currency": {
+      "NeonQuant": 9,
+      "DollarQuant": 9
+    },
+    "LastName": "Harding",
+    "Username": "proident",
+    "FirstName": "West",
+    "PhoneNumber": "occaecat"
+  },
+  {
+    "SSN": "commodo",
+    "email": "ullamco",
+    "Address": "Riegelwood",
+    "Currency": {
+      "NeonQuant": 0,
+      "DollarQuant": 7
+    },
+    "LastName": "Harris",
+    "Username": "tempor",
+    "FirstName": "Ayala",
+    "PhoneNumber": "voluptate"
+  },
+  {
+    "SSN": "est",
+    "email": "qui",
+    "Address": "Gibsonia",
+    "Currency": {
+      "NeonQuant": 4,
+      "DollarQuant": 4
+    },
+    "LastName": "Kelly",
+    "Username": "nostrud",
+    "FirstName": "Victoria",
+    "PhoneNumber": "elit"
+  },
+  {
+    "SSN": "labore",
+    "email": "anim",
+    "Address": "Bynum",
+    "Currency": {
+      "NeonQuant": 6,
+      "DollarQuant": 8
+    },
+    "LastName": "Lyons",
+    "Username": "eu",
+    "FirstName": "Alyce",
+    "PhoneNumber": "sit"
+  },
+  {
+    "SSN": "fugiat",
+    "email": "excepteur",
+    "Address": "Maury",
+    "Currency": {
+      "NeonQuant": 7,
+      "DollarQuant": 10
+    },
+    "LastName": "Black",
+    "Username": "eu",
+    "FirstName": "Agnes",
+    "PhoneNumber": "nostrud"
+  }
+]
